6990b982fe776127400ea9983617fd986a68a265
smsma11 <mutluismail49@gmail.com>
2022-12-11 01:33:50 +0300

    this is my second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Button from "./buttons/likeButton"
-
+import Button from "./buttons/likeButton";
+import Header from "./components/header/header";
+import Footer from "./components/footer/Footer";
+import Main from "./components/main/Main";
 function App() {
   return (
     <div className="App">
       <Button  />
+        <Header title="My header" subtitle="angaragücü" />
+        <Footer note="Footer Note" />
+      <Main message="My content" />
     </div>
   );
 }
-
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+export default function Header(props) {
+    return (
+        <div className="header">
+            <h1>{props.title}</h1>
+            <p>{props.subtitle}</p>
+        </div>
+    );
+}
